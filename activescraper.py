0cbb5c05261438419088541417fb934419bfdd18
Yu Zen <cheahyuzen@gmail.com>
2024-09-15 19:19:54 +0800

    identified the html element that holds the capacity of the activesg facilities and scraped them all, although we will only be focusing on the capacity of the 2nd last facility, Yishun ActiveSG gym.

--- activescraper.py
+++ activescraper.py
@@ -5,11 +5,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-
 driver_service = Service(executable_path=r"C:\Users\Yu Zen\Documents\Coding\chromedriver-win64\chromedriver.exe")
 driver = webdriver.Chrome(service=driver_service)
 
 url = r'https://crowdcapacity.net/'
 driver.get(url)
-driver.implicitly_wait(10)
+driver.implicitly_wait(2)
+lines = driver.find_elements(By.CSS_SELECTOR,"span[style='display: flex; align-items: center; justify-content: center;'][class='badge-light badge bg-success sgds']")
+for line in lines:
+    print(line.text)
 driver.quit()
+
+
+
+
+
