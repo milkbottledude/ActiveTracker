a8ec6f685ec0137cb7eca579a1275e14838b3dc7
Yu Zen <cheahyuzen@gmail.com>
2024-09-22 19:12:20 +0800

    added the csv file for the scraped data to be appended to as well as changed up the format of the code to suit PythonAnywhere paths

--- activescraper.py
+++ activescraper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import csv
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -13,11 +14,11 @@ driver.get(url)
 driver.implicitly_wait(2)
 lines = driver.find_elements(By.CSS_SELECTOR,"span[style='display: flex; align-items: center; justify-content: center;'][class='badge-light badge bg-success sgds']")
 yishuncap = lines[-2].text
-print(f'Yishun Gym capacity: {yishuncap}')
+timestamp = datetime.now().strftime("%A %Y-%m-%d %H:%M:%S")
+to_add = pd.DataFrame({timestamp: [yishuncap]})
+to_add = to_add.T
+to_add.to_csv(r"/home/milkbottledude/Project-activetrack/data.csv", mode='a', header=False, index=True)
 driver.quit()
-
-
-
 
 
 
